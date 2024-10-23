c9bc370e022311963a1a7c7cfa596ee9234cadf7
Samuel Kang <samuel.june.kang@gmail.com>
2024-10-22 22:37:52 -0700

    changed base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/new-site/',
+  base: '/',
   resolve: {
     alias: {
       '@': '/src',
